39a58e41ed474d3fdcacea0166f3b5b975fbd914
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2025-11-30 21:03:26 +0530

    Time: 10 ms (49.91%), Space: 57.6 MB (12.06%) - LeetHub

--- 0851-loud-and-rich/0851-loud-and-rich.java
+++ 0851-loud-and-rich/0851-loud-and-rich.java
@@ -1,56 +1,37 @@
 class Solution {
-
-    public static int dfs(int node, int poor[], int millionaire[],
-                          ArrayList<ArrayList<Integer>> adj,
-                          int quiet[], int[] bestQuiet, int[] bestPerson) {
-
-        if (millionaire[node] != -1) {
-            return millionaire[node];
-        }
-
-        // initialize
-        bestQuiet[node] = quiet[node];
-        bestPerson[node] = node;
-
-        for (int ele : adj.get(node)) {
-            int rich = dfs(ele, poor, millionaire, adj, quiet, bestQuiet, bestPerson);
-
-            if (bestQuiet[rich] < bestQuiet[node]) {
-                bestQuiet[node] = bestQuiet[rich];
-                bestPerson[node] = rich;
-            }
-        }
-
-        millionaire[node] = bestPerson[node];
-        return millionaire[node];
-    }
-
     public int[] loudAndRich(int[][] richer, int[] quiet) {
         int n = quiet.length;
-
-        int millionaire[] = new int[n];
-        Arrays.fill(millionaire, -1);
-
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
-
-        int poor[] = new int[n];
-        for (int edge[] : richer) {
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        int indegree[] = new int[n];
+        for(int edge[]:richer){
             int u = edge[0];
             int v = edge[1];
-            adj.get(v).add(u);
-            poor[v]++;
+            adj.get(u).add(v);
+            indegree[v]++;
         }
-
-        int[] bestQuiet = new int[n];
-        int[] bestPerson = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            if (millionaire[i] == -1) {
-                dfs(i, poor, millionaire, adj, quiet, bestQuiet, bestPerson);
+        Queue<Integer> queue = new LinkedList<>();
+        int millionaire[] = new int[n];
+        for(int i=0;i<n;i++){
+            millionaire[i] = i;
+            if(indegree[i] == 0){
+                queue.add(i);
             }
         }
-
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+            for(int nei :adj.get(node)){
+                if(quiet[millionaire[node]] < quiet[millionaire[nei]]){
+                    millionaire[nei] = millionaire[node];
+                }
+                indegree[nei]--;
+                if(indegree[nei] == 0){
+                    queue.add(nei);
+                }
+            }
+        }
         return millionaire;
     }
 }
