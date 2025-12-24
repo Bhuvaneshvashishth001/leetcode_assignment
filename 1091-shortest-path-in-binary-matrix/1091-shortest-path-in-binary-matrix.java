0eb536230940cd93ea03a3868cc75b25f58ed15c
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2025-12-24 21:38:43 +0530

    Time: 15 ms (70.54%), Space: 45.6 MB (96.82%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,45 +1,52 @@
 class Solution {
-    class Tuple{
+    class tuple{
+        int dist;
         int row;
         int col;
-        int dis;
-        public Tuple(int row,int col,int dis){
-            this.row = row;
-            this.col = col;
-            this.dis = dis;
+        public tuple(int dist,int row,int col){
+            this.dist=dist;
+            this.row=row;
+            this.col=col;
         }
     }
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        if(grid[0][0] ==1){
-            return -1;
-        }
-        if(n-1 ==0 && m-1 ==0){
+        int n= grid.length;
+        int m= grid[0].length;
+        if(n==1 && m==1 && grid[n-1][m-1]==0){
             return 1;
         }
-        int clone[][] = new int[n][m];
-        for(int edge[]:clone){
-            Arrays.fill(edge,Integer.MAX_VALUE);
+        int visited[][] = new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                visited[i][j] = Integer.MAX_VALUE;
+            }
         }
-        PriorityQueue<Tuple> queue = new PriorityQueue<>((a,b)->a.dis-b.dis);
-        queue.add(new Tuple(0,0,1));
+        Queue<tuple> queue = new LinkedList<>();
+        if(grid[0][0] ==1 || grid[n-1][m-1] ==1){
+            return -1;
+        }
+        queue.add(new tuple(0,0,0));
         int dr[] = {-1,-1,-1,0,0,1,1,1};
-        int dc[] = {-1,0,1,-1,1,-1,0,1};
+        int dc[] = {0,-1,1,-1,1,0,-1,1};
         while(!queue.isEmpty()){
-            Tuple t = queue.poll();
-            int x = t.row;
-            int y = t.col;
-            int dist = t.dis;
-            for(int i=0;i<8;i++){
-                int drow = x+dr[i];
-                int dcol = y+dc[i];
-                if(drow>=0 && drow<n && dcol>=0 && dcol<m && grid[drow][dcol]==0 && clone[drow][dcol] > dist+1){
-                    clone[drow][dcol] = dist+1;
-                    queue.add(new Tuple(drow,dcol,dist+1));
+            tuple t = queue.poll();
+            int dis =t.dist;
+            int row =t.row;
+            int col =t.col;
+            if(row == n-1 && col == m-1){
+                return visited[n-1][m-1]+1;
+            }
+            for(int i=0 ;i<8 ;i++){
+                int drow = row+dr[i];
+                int dcol = col+dc[i];
+                if(drow>=0 && drow<n && dcol>=0 && dcol<m && grid[drow][dcol]==0){
+                    if(visited[drow][dcol]>dis+1){
+                        visited[drow][dcol]=dis+1;
+                        queue.add(new tuple(dis+1,drow,dcol));
+                    }
                 }
             }
         }
-        return clone[n-1][m-1] == Integer.MAX_VALUE ? -1:clone[n-1][m-1];
+        return visited[n-1][m-1]==Integer.MAX_VALUE?-1:visited[n-1][m-1]+1;
     }
 }
