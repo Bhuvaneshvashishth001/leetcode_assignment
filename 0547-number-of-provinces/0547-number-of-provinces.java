27256b6f0eab5db4afd42fbe9a65ea930c00e45f
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2025-12-31 11:27:25 +0530

    Time: 2 ms (48.25%), Space: 46.6 MB (98.62%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,19 +1,53 @@
 class Solution {
-    public static void dfs(int matrix[][],int visited[],int i){  
-        for(int j=0;j<matrix.length ;j++){
-            if(matrix[i][j]==1 && visited[j]==0){
-                visited[j]=1;
-                dfs(matrix,visited,j);
+    class Disjoint{
+        int rank[];
+        int par[];
+        public Disjoint(int n){
+            rank = new int[n];
+            par = new int[n];
+            for(int i=0;i<n;i++){
+                par[i] = i;
+            }
+        }
+        public int find(int node){
+            if(par[node] == node){
+                return node;
+            }
+            return par[node] = find(par[node]);
+        }
+        public void union(int u,int v){
+            int par_u = find(u);
+            int par_v = find(v);
+            if(par_u == par_v){
+                return;
+            }
+            if(rank[par_v]>rank[par_u]){
+                par[par_u] = par_v;
+            }
+            else if(rank[par_u]>rank[par_v]){
+                par[par_v] = par_u; 
+            }
+            else{
+                par[par_v] = par_u;
+                rank[par_u]++;
             }
         }
     }
-    public int findCircleNum(int[][] matrix) {
-        int n = matrix.length;
-        int visited[] = new int[n];
-        int count=0;
-        for(int i=0 ;i<n ;i++){
-            if(visited[i] != 1){
-                dfs(matrix,visited,i);
+    public int findCircleNum(int[][] conn) {
+        int n = conn.length;
+        int m = conn[0].length;
+        Disjoint ds = new Disjoint(n);
+        int count = 0;
+        int visit[] = new int[n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(conn[i][j] ==1){
+                    ds.union(i,j);
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(ds.find(i) ==i){
                 count++;
             }
         }
