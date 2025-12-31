a9f3a2cad9114e21e6e7ed4c32ad7e1084f7cf49
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2025-12-31 15:21:53 +0530

    Time: 10 ms (85.33%), Space: 49.7 MB (35.47%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,45 +1,47 @@
 class Solution {
     class Pair{
         int node;
-        int time;
-        public Pair(int node,int time){
+        int dis;
+        public Pair(int node,int dis){
             this.node = node;
-            this.time = time;
+            this.dis = dis;
         }
     }
     public int networkDelayTime(int[][] times, int n, int k) {
         ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-        for(int i=0;i<n+1;i++){
+        for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
         for(int edge[]:times){
-            int u = edge[0];
-            int v = edge[1];
+            int u =edge[0]-1;
+            int v = edge[1]-1;
             int w = edge[2];
             adj.get(u).add(new Pair(v,w));
         }
-        PriorityQueue<Pair> queue = new PriorityQueue<>((a,b)->a.time-b.time);
-        int dist[] = new int[n+1];
+        int dist[] = new int[n];
         Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[k]=0;
-        queue.add(new Pair(k,0));
+        dist[k-1] =0;
+        PriorityQueue<Pair> queue = new PriorityQueue<>((a,b)->a.dis-b.dis);
+        queue.add(new Pair(k-1,0));
         while(!queue.isEmpty()){
             Pair p = queue.poll();
-            int des = p.node;
-            int t = p.time; 
-            for(Pair it:adj.get(des)){
-                if(dist[it.node] > (t+it.time)){
-                    dist[it.node] = t+it.time;
-                    queue.add(new Pair(it.node,t+it.time));
+            int v = p.node;
+            int d = p.dis;
+            for(Pair it:adj.get(v)){
+                if(dist[it.node]>it.dis+d){
+                    dist[it.node]  = it.dis+d;
+                    queue.add(new Pair(it.node,dist[it.node]));
                 }
             }
         }
         int ans = 0;
-        for(int i=1;i<n+1;i++){
-            if(dist[i] == Integer.MAX_VALUE){
+        for(int i=0;i<n;i++){
+            if(dist[i]== Integer.MAX_VALUE){
                 return -1;
             }
-            ans = Math.max(dist[i],ans);
+            else{
+                ans= Math.max(ans,dist[i]);
+            }
         }
         return ans;
     }
