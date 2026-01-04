6a7c1113e3b224204c2f0658aff0019b11810a01
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-01-04 20:04:46 +0530

    Time: 4 ms (99.76%), Space: 55.6 MB (99.91%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,31 +1,26 @@
-class Solution {
-    public List<Integer> eventualSafeNodes(int[][] graph) {
-        int n = graph.length;
-        int state[] = new int[n];
-        int marked[] = new int[n];
-        ArrayList<Integer> list = new ArrayList<>();
+class Solution{
+    public List<Integer> eventualSafeNodes(int[][] graph){
+        int n=graph.length;
+        int[] state=new int[n];
+        List<Integer> result=new ArrayList<>();
         for(int i=0;i<n;i++){
-            if(marked[i] ==1){
-                list.add(i);
-            }
-            if(marked[i] ==0 && dfs(i,graph,state,marked)){
-                list.add(i);
+            if(dfs(i,graph,state)){
+                result.add(i);
             }
         }
-        return list;
+        return result;
     }
-    public static boolean dfs(int node,int graph[][],int state[],int marked[]){
+    private boolean dfs(int node,int[][] graph,int[] state){
         if(state[node]>0){
-            return state[node] ==2;
+            return state[node]==2;
         }
-        state[node] =1;
-        for(int ele : graph[node]){
-            if(!dfs(ele,graph,state,marked)){
+        state[node]=1;
+        for(int nei:graph[node]){
+            if(!dfs(nei,graph,state)){
                 return false;
             }
         }
-        state[node] =2;
-        marked[node]=1;
+        state[node]=2;
         return true;
     }
 }
