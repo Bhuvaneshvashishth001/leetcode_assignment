379e167bf4e1a88694798cbe4d581ca397fdb6c5
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-01-10 18:49:13 +0530

    Time: 91 ms (75.68%), Space: 162.6 MB (75.48%) - LeetHub

--- 1488-avoid-flood-in-the-city/1488-avoid-flood-in-the-city.java
+++ 1488-avoid-flood-in-the-city/1488-avoid-flood-in-the-city.java
@@ -3,21 +3,21 @@ class Solution {
         int n = rains.length;
         int[] ans = new int[n];
         HashMap<Integer, Integer> lastRain = new HashMap<>();
-        TreeSet<Integer> dryDays = new TreeSet<>();
+        TreeSet<Integer> dry = new TreeSet<>();
         for (int i = 0; i < n; i++) {
             if(rains[i] > 0){
                 int lake = rains[i];
                 ans[i] = -1;
                 if (lastRain.containsKey(lake)) {
-                    Integer dryDay = dryDays.ceiling(lastRain.get(lake) + 1);
+                    Integer dryDay = dry.ceiling(lastRain.get(lake) + 1);
                     if (dryDay == null) return new int[0];
                     ans[dryDay] = lake;
-                    dryDays.remove(dryDay);
+                    dry.remove(dryDay);
                 }
                 lastRain.put(lake, i);
             } 
             else{
-                dryDays.add(i);
+                dry.add(i);
                 ans[i] = 1;
             }
         }
