4bcf13b58d15bb96ce818675d94c94962b5ab855
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-01-10 20:42:59 +0530

    Time: 36 ms (93.33%), Space: 80.1 MB (13.33%) - LeetHub

---
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int longestSubsequence(int[] arr, int diff) {
+        HashMap<Integer, Integer> dp = new HashMap<>();
+        int ans = 1;
+        for (int x : arr) {
+            int prev = x - diff;
+            int currLen = dp.getOrDefault(prev, 0) + 1;
+            dp.put(x, currLen);
+            ans = Math.max(ans, currLen);
+        }
+        return ans;
+    }
+}
