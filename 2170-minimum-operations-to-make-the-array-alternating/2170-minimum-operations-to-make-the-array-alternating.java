b89a5f1f29d1fcd09b63ec691c48154ccb768048
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-01-18 19:36:37 +0530

    Time: 112 ms (16.07%), Space: 113.5 MB (23.22%) - LeetHub

--- 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.java
+++ 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.java
@@ -1,38 +1,36 @@
 class Solution {
     public int minimumOperations(int[] nums) {
         int n = nums.length;
-        if(n==1) return 0;
-        HashMap<Integer,Integer> even = new HashMap<>();
         HashMap<Integer,Integer> odd = new HashMap<>();
+        HashMap<Integer,Integer> even = new HashMap<>();
+        int o= 0;
+        int e = 0;
         for(int i=0;i<n;i++){
-            if(i%2==0){
-                even.put(nums[i],even.getOrDefault(nums[i],0)+1);
-            }else{
+            if(i%2 ==0){
                 odd.put(nums[i],odd.getOrDefault(nums[i],0)+1);
+                o++;
+            }
+            else{
+                even.put(nums[i],even.getOrDefault(nums[i],0)+1);
+                e++;
             }
         }
-        int e[] = get(even);
-        int o[] = get(odd);
-        if(e[0]!=o[0]){
-            return (n+1)/2 - e[1] + n/2 - o[1];
-        }else{
-            int a = (n+1)/2 - e[1] + n/2 - o[3];
-            int b = (n+1)/2 - e[3] + n/2 - o[1];
-            return Math.min(a,b);
+        ArrayList<Map.Entry<Integer,Integer>> list1 = new ArrayList<>(odd.entrySet());
+        ArrayList<Map.Entry<Integer,Integer>> list2 = new ArrayList<>(even.entrySet());
+        list1.sort((a,b)->b.getValue() - a.getValue());
+        list2.sort((a,b)->b.getValue() - a.getValue());
+        int ans = Integer.MAX_VALUE;
+        if(!list1.isEmpty() && !list2.isEmpty() && !list1.get(0).getKey().equals(list2.get(0).getKey())){
+            ans = Math.min(ans,o-(list1.get(0).getValue())+(e-list2.get(0).getValue()));
         }
-    }
-    int[] get(HashMap<Integer,Integer> mp){
-        int v1=-1,c1=0;
-        int v2=-1,c2=0;
-        for(int k:mp.keySet()){
-            int c = mp.get(k);
-            if(c>c1){
-                c2=c1; v2=v1;
-                c1=c;  v1=k;
-            }else if(c>c2){
-                c2=c; v2=k;
+        else{
+            if(list1.size()>1){
+                ans = Math.min(ans,(o-list1.get(1).getValue())+(e-list2.get(0).getValue()));
+            }
+            if(list2.size()>1){
+                ans = Math.min(ans,(o- list1.get(0).getValue())+(e- list2.get(1).getValue()));
             }
         }
-        return new int[]{v1,c1,v2,c2};
+        return ans==Integer.MAX_VALUE ? Math.min(e,o):ans;
     }
 }
