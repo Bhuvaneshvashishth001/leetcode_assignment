f45f996323033e555463ed8fc66850bc8830dad7
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-01-20 19:29:44 +0530

    Time: 18 ms (54.94%), Space: 44.5 MB (30.8%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.java
+++ 1395-count-number-of-teams/1395-count-number-of-teams.java
@@ -1,41 +1,37 @@
 class Solution {
     public int numTeams(int[] arr) {
         int n = arr.length;
-
-        int greaterRight[] = new int[n];
-        int smallerRight[] = new int[n];
-
-        // count right side
-        for (int i = n - 1; i >= 0; i--) {
+        int max[] = new int[n];
+        int min[] = new int[n];
+        for(int i=0;i<n;i++){
             int gr = 0;
             int sr = 0;
-
-            for (int j = i + 1; j < n; j++) {
-                if (arr[i] < arr[j]) gr++;
-                else if (arr[i] > arr[j]) sr++;
+            for(int j=i+1;j<n;j++){
+                if(arr[i]>arr[j]){
+                    sr++;
+                }
+                if(arr[i]<arr[j]){
+                    gr++;
+                }
             }
-
-            greaterRight[i] = gr;
-            smallerRight[i] = sr;
+            max[i] = gr;
+            min[i] = sr;
         }
-
-        int ans = 0;
-
-        // count left side + form teams
-        for (int j = 0; j < n; j++) {
-            int smallerLeft = 0;
-            int greaterLeft = 0;
-
-            for (int i = 0; i < j; i++) {
-                if (arr[i] < arr[j]) smallerLeft++;
-                else if (arr[i] > arr[j]) greaterLeft++;
+        int ans =0;
+        for(int i=0;i<n;i++){
+            int gr = 0;
+            int sr = 0;
+            for(int j=0;j<i;j++){
+                if(arr[i]<arr[j]){
+                    gr++;
+                }
+                if(arr[i]>arr[j]){
+                    sr++;
+                }
             }
-
-            // increasing + decreasing
-            ans += smallerLeft * greaterRight[j];
-            ans += greaterLeft * smallerRight[j];
+            ans += max[i]*sr;
+            ans += min[i]*gr;
         }
-
         return ans;
     }
 }
