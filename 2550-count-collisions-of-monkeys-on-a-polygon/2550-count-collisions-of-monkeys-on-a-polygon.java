0236af4f5821de13deb2c4dd098febee8cd4a247
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-01-22 23:59:27 +0530

    Time: 0 ms (100%), Space: 41.9 MB (92.27%) - LeetHub

---
+++ 2550-count-collisions-of-monkeys-on-a-polygon/2550-count-collisions-of-monkeys-on-a-polygon.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    int mod = 1_000_000_007;
+
+    public long sol(long x, int n) {
+        if (n == 0) return 1;
+
+        long half = sol(x, n / 2);
+
+        long ans = (half * half) % mod;
+
+        if (n % 2 != 0) {
+            ans = (ans * x) % mod;
+        }
+
+        return ans;
+    }
+
+    public int monkeyMove(int n) {
+
+        long total = sol(2, n);
+
+        return (int)((total - 2 + mod) % mod);
+    }
+}
