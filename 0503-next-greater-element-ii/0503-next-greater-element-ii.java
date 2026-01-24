badd90c9cad3013974f8fd40693d171c79ea4e11
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-01-24 12:30:30 +0530

    Time: 5 ms (93.59%), Space: 48.8 MB (16.28%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,22 +1,22 @@
-class Solution {
-    public int[] nextGreaterElements(int[] nums) {
-        int n = nums.length;
-        int ans[] = new int[n];
-        Stack<Integer> stack = new Stack<>();
-        for(int i=2*n-1;i>=0;i--){
-            while(!stack.isEmpty() && stack.peek()<=nums[i%n]){
-                stack.pop();
-            }
-            if(i<n){
-                if(stack.isEmpty()){
-                    ans[i] = -1;
+class Solution { 
+    public int[] nextGreaterElements(int[] nums) { 
+        int n = nums.length; 
+        ArrayDeque <Integer> stack = new ArrayDeque<>(); 
+        int map[] = new int[n]; 
+        for(int i=2*n-1; i>=0 ;i--){ 
+            while(!stack.isEmpty() && nums[i%n]>=stack.peek()){ 
+                stack.pop(); 
+            } 
+            if(i<n){ 
+                if(stack.isEmpty()){ 
+                    map[i] = -1; 
+                } 
+                else{ 
+                    map[i] = stack.peek(); 
                 }
-                else{
-                    ans[i] = stack.peek();
-                }
             }
-            stack.push(nums[i%n]);
-        }  
-        return ans;      
-    }
+            stack.push(nums[i%n]); 
+        } 
+        return map; 
+    } 
 }
