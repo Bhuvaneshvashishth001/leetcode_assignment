6f2739fe8ea4afe16d175165618a0f175d237fdf
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-01-26 23:18:18 +0530

    Time: 408 ms (58.1%), Space: 89.5 MB (21.79%) - LeetHub

--- 3589-count-prime-gap-balanced-subarrays/3589-count-prime-gap-balanced-subarrays.java
+++ 3589-count-prime-gap-balanced-subarrays/3589-count-prime-gap-balanced-subarrays.java
@@ -1,52 +1,49 @@
-import java.util.*;
-
 class Solution {
-
     int prime[] = new int[50001];
-
-    public void sieve() {
-        prime[0] = 1;
-        prime[1] = 1;
-        for (int i = 2; i * i < prime.length; i++) {
-            if (prime[i] == 0) {
-                for (int j = i * i; j < prime.length; j += i) {
-                    prime[j] = 1;
+    public void sieve(){
+        int  n = prime.length;
+        prime[0] =1;
+        prime[1] =1;
+        for(int i=2;i*i<n;i++){
+            if(prime[i] ==0){
+                for(int j=i*i;j<n;j+=i){
+                    prime[j] =1;
                 }
             }
         }
     }
-
     public int primeSubarray(int[] nums, int k) {
-
         sieve();
-        int n = nums.length;
-        PriorityQueue<int[]> minQ =new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        PriorityQueue<int[]> maxQ =new PriorityQueue<>((a, b) -> b[0] - a[0]);
-        Deque<Integer> primeIdx = new ArrayDeque<>();
-        int i = 0;
-        int ans = 0;
-        for (int j = 0; j < n; j++) {
-            if (prime[nums[j]] == 0) {
-                minQ.add(new int[]{nums[j], j});
-                maxQ.add(new int[]{nums[j], j});
-                primeIdx.addLast(j);
+        int m = nums.length;
+        PriorityQueue<int[]>min = new PriorityQueue<>((a,b)->a[0]-b[0]);
+        PriorityQueue<int[]>max = new PriorityQueue<>((a,b)->b[0]-a[0]);
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        int i=0;
+        int ans =0;
+        for(int j=0;j<m;j++){
+            if(prime[nums[j]] ==0){
+                min.add(new int[]{nums[j],j});
+                max.add(new int[]{nums[j],j});
+                queue.addLast(j);
             }
-            while (primeIdx.size() >= 2 &&
-                   maxQ.peek()[0] - minQ.peek()[0] > k) {
-                if (!primeIdx.isEmpty() && primeIdx.peekFirst() == i)
-                    primeIdx.pollFirst();
+            while(queue.size()>=2 && max.peek()[0]-min.peek()[0] >k){
+                if(!queue.isEmpty() && queue.peekFirst() ==i){
+                    queue.pollFirst();
+                }
                 i++;
-                while (!minQ.isEmpty() && minQ.peek()[1] < i)
-                    minQ.poll();
-                while (!maxQ.isEmpty() && maxQ.peek()[1] < i)
-                    maxQ.poll();
+                while(!min.isEmpty() && min.peek()[1]<i){
+                    min.poll();
+                }
+                while(!max.isEmpty() && max.peek()[1]<i){
+                    max.poll();
+                }
             }
-            if (primeIdx.size() >= 2) {
-                int secondLast = primeIdx.peekLast();
-                primeIdx.pollLast();
-                int firstLast = primeIdx.peekLast();
-                primeIdx.addLast(secondLast);
-                ans += (firstLast - i + 1);
+            if(queue.size()>=2){
+                int second = queue.peekLast();
+                queue.pollLast();
+                int first = queue.peekLast();
+                queue.addLast(second);
+                ans += first-i+1;
             }
         }
         return ans;
