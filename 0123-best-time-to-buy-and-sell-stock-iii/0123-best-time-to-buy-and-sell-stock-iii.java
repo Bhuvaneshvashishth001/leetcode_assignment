57fbe91e4c85b26e6a2305e557e7d0f3678c1550
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-01-28 05:41:20 +0530

    Time: 1 ms (99.49%), Space: 76.1 MB (63.69%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,33 +1,15 @@
 class Solution {
-    public int rec(int idx,int canBuy,int arr[],int trans,int dp[][][]){
-        if(idx >= arr.length || trans>=2){
-            return 0;
-        }
-        if(dp[idx][canBuy][trans]!=-1){
-            return dp[idx][canBuy][trans];
-        }
-        int pro=0;
-        if(canBuy == 1){
-            int buy = -arr[idx]+rec(idx+1,0,arr,trans,dp);
-            int skip = rec(idx+1,1,arr,trans,dp);
-            pro = Math.max(buy,skip);
-        }
-        else{
-            int sell = arr[idx]+rec(idx+1,1,arr,trans+1,dp);
-            int skip = rec(idx+1,0,arr,trans,dp);
-            pro = Math.max(sell,skip);
-        }
-        return dp[idx][canBuy][trans] = pro;
-    }
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int dp[][][] = new int[n][2][2];
-        for(int row[][]:dp){
-            for(int d[]:row){
-                Arrays.fill(d,-1);
-            }
+        int buy1 = Integer.MIN_VALUE;
+        int sell1 = 0;
+        int buy2 = Integer.MIN_VALUE;
+        int sell2 = 0;
+        for (int price : prices) {
+            buy1 = Math.max(buy1, -price);
+            sell1 = Math.max(sell1, buy1 + price);
+            buy2 = Math.max(buy2, sell1 - price);
+            sell2 = Math.max(sell2, buy2 + price);
         }
-        int canBuy = 1;
-        return rec(0,canBuy,prices,0,dp);
+        return sell2;
     }
 }
