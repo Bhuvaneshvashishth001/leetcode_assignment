d74e55be329297a0326fcdf320a4845415f16e7e
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-03-17 08:19:36 +0530

    Time: 6 ms (71.72%), Space: 45.4 MB (35.43%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,17 +1,47 @@
 class Solution {
-    public int helper(int arr[],int idx,int target){
-        if(idx == arr.length && target ==0){
-            return 1;
+    public int findTargetSumWays(int[] nums, int target) {
+
+        int n = nums.length;
+        int total = 0;
+
+        for(int num : nums){
+            total += num;
         }
-        if(idx >= arr.length){
+
+        int sum = total + target;
+
+        if(sum < 0 || sum % 2 != 0){
             return 0;
         }
-        int count =0;
-        count += helper(arr,idx+1,target-arr[idx]);
-        count += helper(arr,idx+1,target+arr[idx]);
-        return count;
-    }
-    public int findTargetSumWays(int[] nums, int target) {
-        return helper(nums,0,target);
+
+        int amount = sum / 2;
+
+        int dp[][] = new int[n][amount+1];
+
+        // base case
+        if(nums[0] == 0){
+            dp[0][0] = 2;
+        } else {
+            dp[0][0] = 1;
+            if(nums[0] <= amount){
+                dp[0][nums[0]] = 1;
+            }
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+
+                int first = dp[i-1][j];
+
+                int second = 0;
+                if(j - nums[i] >= 0){
+                    second = dp[i-1][j - nums[i]];
+                }
+
+                dp[i][j] = first + second;
+            }
+        }
+
+        return dp[n-1][amount];
     }
 }
