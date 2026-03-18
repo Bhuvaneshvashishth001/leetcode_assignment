e6be74c0907cb9c1a7b7ab19f68b3ca3f39fc11f
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-03-18 05:40:18 +0530

    Time: 8 ms (9.19%), Space: 46.8 MB (8.76%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,21 @@
 class Solution {
+    public int helper(int arr[],int i,int j,int dp[][]){
+        if(i>j){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int first = arr[i] - helper(arr,i+1,j,dp);
+        int second = arr[j] - helper(arr,i,j-1,dp);
+        return dp[i][j] = Math.max(first,second);
+    }
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n = piles.length;
+        int dp[][] = new int[n][n];
+        for(int row[]:dp){
+            Arrays.fill(row,-1);
+        }
+        return helper(piles,0,n-1,dp)>0;
     }
 }
