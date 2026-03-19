75363540b4ebbadb5793ce8d51bb361e4d1ca125
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-03-19 12:36:32 +0530

    Time: 8 ms (22.72%), Space: 46.2 MB (89.1%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -1,26 +1,34 @@
 class Solution {
-    public int minDistance(String text1, String text2) {
-        int m = text1.length();
-        int n = text2.length();
-        int[] dp = new int[n+1];
-        for(int i=1;i<=m;i++){
-        int prev = 0;
-            for(int j=1;j<=n;j++){
-                int t = dp[j];
-                if(text1.charAt(i-1)==text2.charAt(j-1)){
-                    dp[j]=1+prev;
-                }
-                else{
-                    dp[j]=Math.max(dp[j],dp[j-1]);
-                }
-            prev =t;
-            }
+    int dp[][];
+    public int helper(int i,int j,String s1,String s2){
+        if(i == s1.length() && j < s2.length()){
+            return s2.length()-j;
         }
-        if(dp[n]==text2.length()){
-            return text1.length()-dp[n];
+        if(i<s1.length() && j==s2.length()){
+            return s1.length()-i;
+        }
+        if(i == s1.length() && j == s2.length()){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        if(s1.charAt(i) == s2.charAt(j)){
+            return dp[i][j] = helper(i+1,j+1,s1,s2);
         }
         else{
-            return text1.length()-dp[n]+text2.length()-dp[n];
+            int first = 1+helper(i+1,j,s1,s2);
+            int second = 1+helper(i,j+1,s1,s2);
+            return dp[i][j] = Math.min(first,second);
         }
+    }
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        dp = new int[n+1][m+1];
+        for(int row[]:dp){
+            Arrays.fill(row,-1);
+        }
+        return helper(0,0,word1,word2);
     }
 }
