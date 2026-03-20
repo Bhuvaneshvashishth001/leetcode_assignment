38ead1064339148cabb139feb2cff33d4274de8f
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-03-20 06:37:16 +0530

    Time: 3 ms (98.08%), Space: 47.1 MB (65.91%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,35 @@
+class Solution {
+    int dp[][];
+    public int helper(int i,int j,String s1,String s2){
+        if(i>= s1.length() && j< s2.length()){
+            return s2.length()-j;
+        }
+        if(j>= s2.length() && i<s1.length()){
+            return s1.length()-i;
+        }
+        if( i== s1.length() && j==s2.length()){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        if(s1.charAt(i) == s2.charAt(j)){
+            return dp[i][j] =  helper(i+1,j+1,s1,s2);
+        }
+        else{
+            int ins = 1+helper(i,j+1,s1,s2);
+            int del = 1+helper(i+1,j,s1,s2);
+            int rep = 1+helper(i+1,j+1,s1,s2);
+            return dp[i][j] = Math.min(ins,Math.min(rep,del));
+        }
+    }
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        dp = new int[n+1][m+1];
+        for(int row[] : dp){
+            Arrays.fill(row,-1);
+        }
+        return helper(0,0,word1,word2);
+    }
+}
