60485eb531dc1c86dcdfd26c2ba5dab14516a54f
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-03-31 14:43:31 +0530

    Time: 3 ms (15.6%), Space: 46.7 MB (7.68%) - LeetHub

--- 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.java
+++ 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.java
@@ -1,20 +1,71 @@
 class Solution {
-    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
-        int n = fruits.length;
-        int m = baskets.length;
-        int fill[] = new int[m];
-        int count = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(baskets[j]>=fruits[i] && fill[j] != 1){
-                    fill[j] =1;
-                    break;
-                }
-            }
+    int seg[];
+
+    public void buildST(int i,int l,int r,int arr[]){
+        if(l==r){
+            seg[i] = l;
+            return;
         }
-        for(int i=0;i<m;i++){
-            if(fill[i] != 1){
+        int mid = (l+r)/2;
+        buildST(2*i+1,l,mid,arr);
+        buildST(2*i+2,mid+1,r,arr);
+        int left = seg[2*i+1];
+        int right = seg[2*i+2];
+
+        if(arr[left] >= arr[right]){
+            seg[i] = left;
+        }
+        else{
+            seg[i] = right;
+        }
+    }
+    public int findFirstGreater(int i,int l,int r,int t,int arr[]){
+        if(arr[seg[i]] < t){
+            return -1;
+        } 
+        if(l == r){
+            return l;
+        }
+        int mid = (l + r) / 2;
+        int left = findFirstGreater(2*i+1, l, mid, t, arr);
+        if (left != -1){
+            return left;
+        }
+        return findFirstGreater(2*i+2, mid+1, r, t, arr);
+    }
+    public void update(int i,int l,int r,int idx,int arr[]){
+        if(l==r){
+            arr[idx] = -1; 
+            return;
+        }
+        int mid = (l+r)/2;
+        if(idx <= mid){
+            update(2*i+1,l,mid,idx,arr);
+        }
+        else{
+            update(2*i+2,mid+1,r,idx,arr);
+        }
+        int left = seg[2*i+1];
+        int right = seg[2*i+2];
+        if(arr[left] >= arr[right]){
+            seg[i] = left;
+        }
+        else{
+            seg[i] = right;
+        }
+    }
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int n = baskets.length;
+        seg = new int[4*n];
+        buildST(0,0,n-1,baskets);
+        int count = 0;
+        for(int i=0;i<fruits.length;i++){
+            int idx = findFirstGreater(0,0,n-1,fruits[i],baskets);
+            if(idx == -1){
                 count++;
+            }
+            else{
+                update(0,0,n-1,idx,baskets);
             }
         }
         return count;
