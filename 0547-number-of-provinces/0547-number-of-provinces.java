462d84c1af6e3c8ce0f85916c53e75836e430dd2
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-07-25 17:36:54 +0530

    Time: 0 ms (100%), Space: 47.2 MB (59.44%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,53 +1,20 @@
 class Solution {
-    class Disjoint{
-        int rank[];
-        int par[];
-        public Disjoint(int n){
-            rank = new int[n];
-            par = new int[n];
-            for(int i=0;i<n;i++){
-                par[i] = i;
-            }
-        }
-        public int find(int node){
-            if(par[node] == node){
-                return node;
-            }
-            return par[node] = find(par[node]);
-        }
-        public void union(int u,int v){
-            int par_u = find(u);
-            int par_v = find(v);
-            if(par_u == par_v){
-                return;
-            }
-            if(rank[par_v]>rank[par_u]){
-                par[par_u] = par_v;
-            }
-            else if(rank[par_u]>rank[par_v]){
-                par[par_v] = par_u; 
-            }
-            else{
-                par[par_v] = par_u;
-                rank[par_u]++;
+    public void dfs(int node ,int [][] isConnected,boolean vis[]){
+        int m  = isConnected[0].length;
+        vis[node] = true;
+        for(int i=0;i<m;i++){
+            if(isConnected[node][i] == 1 && !vis[i]){
+                dfs(i,isConnected,vis);
             }
         }
     }
-    public int findCircleNum(int[][] conn) {
-        int n = conn.length;
-        int m = conn[0].length;
-        Disjoint ds = new Disjoint(n);
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean vis[] = new boolean[n];
         int count = 0;
-        int visit[] = new int[n];
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(conn[i][j] ==1){
-                    ds.union(i,j);
-                }
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(ds.find(i) ==i){
+            if(!vis[i]){
+                dfs(i,isConnected,vis);
                 count++;
             }
         }
