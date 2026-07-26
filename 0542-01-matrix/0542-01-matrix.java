603e9f80092a502fafc08d9d193b73f7ea174c69
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-07-26 23:51:09 +0530

    Time: 14 ms (69.68%), Space: 49 MB (59.64%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,52 +1,45 @@
 class Solution {
-    class Tuple{
+    class Pair{
         int row;
         int col;
         int dis;
-        public Tuple(int row,int col,int dis){
+        public Pair(int row,int col,int dis){
             this.row = row;
             this.col = col;
             this.dis = dis;
         }
     }
     public int[][] updateMatrix(int[][] mat) {
-        int n = mat.length;
+        int n  = mat.length;
         int m = mat[0].length;
-        Queue<Tuple> queue = new LinkedList<>();
-        int dist[][] = new int[n][m];
-        for(int dis[]:dist){
-            Arrays.fill(dis,Integer.MAX_VALUE);
-        }
+        int grid[][] = new int[n][m];
+        int visited[][] = new int[n][m];
+        Queue<Pair> queue = new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j] == 0){
-                    dist[i][j] =0;
-                    queue.add(new Tuple(i,j,0));
+                    queue.add(new Pair(i,j,0));
+                    visited[i][j] =1;
                 }
             }
         }
         int dr[] = {-1,0,0,1};
         int dc[] = {0,-1,1,0};
         while(!queue.isEmpty()){
-            Tuple t = queue.poll();
-            int x = t.row;
-            int y = t.col;
-            int d = t.dis;
-            if(dist[x][y] <d){
-                continue;
-            }
+            Pair p = queue.poll();
+            int x = p.row;
+            int y = p.col;
+            int step = p.dis;
             for(int i=0;i<4;i++){
                 int drow = x+dr[i];
                 int dcol = y+dc[i];
-                if(drow>=0 && drow<n &&  dcol>=0 && dcol<m && mat[drow][dcol] ==1){
-                    if(dist[drow][dcol] > d+1){
-                        dist[drow][dcol] = d+1;
-                        queue.add(new Tuple(drow,dcol,d+1));
-                    }
+                if(drow>= 0 && drow<n && dcol>=0 && dcol<m && visited[drow][dcol] == 0){
+                    mat[drow][dcol] = step+1;
+                    queue.add(new Pair(drow,dcol,step+1));
+                    visited[drow][dcol] = 1;
                 }
             }
         }
-        
-        return dist;
-    }
+        return mat;
+     }
 }
