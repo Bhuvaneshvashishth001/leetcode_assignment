b6f5c70d9be5f64f89a67f60b3f49a3f0538285b
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-07-29 17:26:14 +0530

    Time: 4 ms (99.61%), Space: 63.3 MB (39.22%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,36 +1,26 @@
-class Solution {
-    public List<Integer> eventualSafeNodes(int[][] graph) {
-        int n = graph.length;
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+class Solution{
+    public List<Integer> eventualSafeNodes(int[][] graph){
+        int n=graph.length;
+        int[] state=new int[n];
+        List<Integer> result=new ArrayList<>();
         for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
-        }
-        int outDegree[] = new int[n];
-        for(int i=0;i<n;i++){
-            for(int v:graph[i]){
-                adj.get(v).add(i);
-                outDegree[i]++;
+            if(dfs(i,graph,state)){
+                result.add(i);
             }
         }
-        Queue<Integer> queue = new LinkedList<>();
-        List<Integer> ans = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if(outDegree[i] == 0){
-                ans.add(i);
-                queue.add(i);
+        return result;
+    }
+    private boolean dfs(int node,int[][] graph,int[] state){
+        if(state[node]>0){
+            return state[node]==2;
+        }
+        state[node]=1;
+        for(int nei:graph[node]){
+            if(!dfs(nei,graph,state)){
+                return false;
             }
         }
-        while(!queue.isEmpty()){
-            int node = queue.poll();
-            for(int it:adj.get(node)){
-                outDegree[it]--;
-                if(outDegree[it] == 0){
-                    ans.add(it);
-                    queue.add(it);
-                }
-            }
-        }
-        Collections.sort(ans);
-        return ans;
+        state[node]=2;
+        return true;
     }
 }
