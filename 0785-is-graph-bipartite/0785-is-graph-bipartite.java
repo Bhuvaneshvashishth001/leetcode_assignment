f5b2d3c49d269d241140ec8133125dec6323f0b5
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-07-29 11:45:58 +0530

    Time: 6 ms (9.85%), Space: 47.2 MB (38.02%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,43 +1,56 @@
 class Solution {
-    public static boolean dfs(int node,ArrayList<ArrayList<Integer>>adj,int visited[],int color){
-        visited[node] = color;
-        for(int ele : adj.get(node)){
-            if(visited[ele] == -1){
-                if(dfs(ele,adj,visited,1-color)){
-                    return true;
-                }
+
+    public boolean dfs(int node, ArrayList<ArrayList<Integer>> adj, int mark[]) {
+
+        for (int it : adj.get(node)) {
+
+            if (mark[node] == mark[it]) {
+                return false;
             }
-            else{
-                if(visited[ele] == visited[node]){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    public boolean isBipartite(int[][] graph) {
-        int n = graph.length;
-        ArrayList<ArrayList<Integer>>adj = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
-        }
-        int j=0;
-        for(int edge[] : graph){
-            for(int ele:edge){
-                adj.get(j).add(ele);
-            }
-            j++;
-        }
-        int color=1;
-        int visited[] = new int[n];
-        Arrays.fill(visited,-1);
-        for(int i=0;i<n;i++){
-            if(visited[i] == -1){
-                if(dfs(i,adj,visited,color)){
+
+            if (mark[it] == 0) {
+
+                if (mark[node] == 1)
+                    mark[it] = 2;
+                else
+                    mark[it] = 1;
+
+                if (!dfs(it, adj, mark))
                     return false;
-                }
             }
         }
+
+        return true;
+    }
+
+    public boolean isBipartite(int[][] graph) {
+
+        int n = graph.length;
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < n; i++)
+            adj.add(new ArrayList<>());
+
+        for (int i = 0; i < n; i++) {
+            for (int num : graph[i]) {
+                adj.get(i).add(num);
+            }
+        }
+
+        int mark[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+
+            if (mark[i] == 0) {
+
+                mark[i] = 1;
+
+                if (!dfs(i, adj, mark))
+                    return false;
+            }
+        }
+
         return true;
     }
 }
