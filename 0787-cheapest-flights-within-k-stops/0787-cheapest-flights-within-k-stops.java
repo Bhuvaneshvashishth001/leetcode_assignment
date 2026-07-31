b5543960a9f2c5277cac64e6edc66573560c8616
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-07-31 18:25:26 +0530

    Time: 9 ms (21.92%), Space: 46.3 MB (84.7%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,37 +1,53 @@
-import java.util.*;
-
 class Solution {
-    class Pair {
+    class Tuple{
+        int src;
+        int spend;
+        int stop;
+        public Tuple(int src,int spend,int stop){
+            this.src = src;
+            this.spend = spend;
+            this.stop = stop;
+        }
+    }
+    class Pair{
         int node;
         int price;
-        int stop;
-        public Pair(int node, int price, int stop) {
+        public Pair(int node,int price){
             this.node = node;
             this.price = price;
-            this.stop = stop;
         }
     }
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
-        for (int[] edge : flights) adj.get(edge[0]).add(new Pair(edge[1], edge[2], 0));
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.price - b.price);
-        pq.add(new Pair(src, 0, 0));
-        int[][] dist = new int[n][k + 2]; 
-        for (int[] row : dist) Arrays.fill(row, Integer.MAX_VALUE);
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int edge[] : flights){
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+            adj.get(u).add(new Pair(v,w));
+        }
+        int dist[][] = new int[n][k+2];
+        for(int row[]:dist){
+            Arrays.fill(row,Integer.MAX_VALUE);
+        }
+        PriorityQueue<Tuple> queue = new PriorityQueue<>((a,b)->a.spend - b.spend);
+        queue.add(new Tuple(src,0,0));
         dist[src][0] = 0;
-        while (!pq.isEmpty()) {
-            Pair cur = pq.poll();
-            int node = cur.node;
-            int price = cur.price;
-            int stops = cur.stop;
-            if (node == dst) return price;
-            if (stops <= k) {
-                for (Pair nei : adj.get(node)) {
-                    int newPrice = price + nei.price;
-                    if (newPrice < dist[nei.node][stops + 1]) {
-                        dist[nei.node][stops + 1] = newPrice;
-                        pq.add(new Pair(nei.node, newPrice, stops + 1));
+        while(!queue.isEmpty()){
+            Tuple t = queue.poll();
+            int node = t.src;
+            int price = t.spend;
+            int stops = t.stop;
+            if(node == dst){
+                return price;
+            }
+            if(stops<=k){
+                for(Pair p : adj.get(node)){
+                    if(dist[p.node][stops+1] > price+p.price){
+                        dist[p.node][stops+1] = price+p.price;
+                        queue.add(new Tuple(p.node,price+p.price,stops+1));
                     }
                 }
             }
