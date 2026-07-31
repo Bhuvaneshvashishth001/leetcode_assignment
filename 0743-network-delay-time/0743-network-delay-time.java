b10b62dbf3fe3b4f6194293db705ecace0742158
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-07-31 18:57:03 +0530

    Time: 10 ms (80.2%), Space: 49.4 MB (30.55%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,48 +1,46 @@
 class Solution {
-    class Pair{
-        int node;
-        int dis;
-        public Pair(int node,int dis){
-            this.node = node;
-            this.dis = dis;
+    class Tuple{
+        int src;
+        int spend;
+        public Tuple(int src,int spend){
+            this.src = src;
+            this.spend = spend;
         }
     }
     public int networkDelayTime(int[][] times, int n, int k) {
-        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
         for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-        for(int edge[]:times){
-            int u =edge[0]-1;
+        for(int edge[] : times){
+            int u = edge[0]-1;
             int v = edge[1]-1;
             int w = edge[2];
-            adj.get(u).add(new Pair(v,w));
+            adj.get(u).add(new int[]{v,w});
         }
-        int dist[] = new int[n];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[k-1] =0;
-        PriorityQueue<Pair> queue = new PriorityQueue<>((a,b)->a.dis-b.dis);
-        queue.add(new Pair(k-1,0));
+        int spent[] = new int[n];
+        Arrays.fill(spent, Integer.MAX_VALUE);
+        PriorityQueue<Tuple> queue = new PriorityQueue<>((a,b)->a.spend-b.spend);
+        spent[k-1] = 0;
+        queue.add(new Tuple(k-1,0));
         while(!queue.isEmpty()){
-            Pair p = queue.poll();
-            int v = p.node;
-            int d = p.dis;
-            for(Pair it:adj.get(v)){
-                if(dist[it.node]>it.dis+d){
-                    dist[it.node]  = it.dis+d;
-                    queue.add(new Pair(it.node,dist[it.node]));
+            Tuple t = queue.poll();
+            int src = t.src;
+            int spend = t.spend;
+            for(int edge[] : adj.get(src)){
+                if(spent[edge[0]] > spend+edge[1]){
+                    spent[edge[0]] = spend+edge[1];
+                    queue.add(new Tuple(edge[0],spent[edge[0]]));
                 }
             }
         }
-        int ans = 0;
+        int max = 0;
         for(int i=0;i<n;i++){
-            if(dist[i]== Integer.MAX_VALUE){
+            if(spent[i] == Integer.MAX_VALUE){
                 return -1;
             }
-            else{
-                ans= Math.max(ans,dist[i]);
-            }
+            max = Math.max(max,spent[i]);
         }
-        return ans;
+        return max;
     }
 }
