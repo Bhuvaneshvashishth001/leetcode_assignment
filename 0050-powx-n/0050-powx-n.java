41478d2c4e93f0c75b6079e99e2a2a47aaa5a597
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-08-04 19:42:22 +0530

    Time: 0 ms (100%), Space: 48.4 MB (11.54%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,6 +1,27 @@
 class Solution {
-    public double myPow(double x, int n) {
-        double ans = Math.pow(x,n);
+    public double helper(double x,int n){
+        if(n==0){
+            return 1.0;
+        }
+        double half = helper(x,n/2);
+        double ans =  half*half;
+        if(n%2 != 0){
+            ans = ans*x;
+        }
         return ans;
+    }
+    public double myPow(double x, int n) {
+        if(x == 0.0){
+            return 0.0;
+        }
+        if(n== 0){
+            return 1.0;
+        }
+        int num = n;
+        if(num < 0){
+            x = 1/x;
+            num = -num;
+        }
+        return helper(x,num);
     }
 }
