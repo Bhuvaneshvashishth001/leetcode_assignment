fc988ae02f820cd4bf73218ea953375e27db1613
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-08-04 18:59:39 +0530

    Time: 0 ms (100%), Space: 48.2 MB (34.95%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,24 +1,6 @@
 class Solution {
-    public double sol(double x,long n){
-        if(n ==0){
-            return 1.0;
-        }
-        double half = sol(x,n/2);
-        double ans = half*half;
-        if(n%2 !=0){
-            ans *=x;
-        }
-        return ans;
-    }
     public double myPow(double x, int n) {
-        if(x == 0.0){
-            return 0.0;
-        }
-        long num = n;
-        if(num<0){
-            x= 1/x;
-            num = -num;
-        }
-        return sol(x,num);
+        double ans = Math.pow(x,n);
+        return ans;
     }
 }
