69743a791909e739659d84fb894be51c1a6ccf93
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-08-11 17:09:07 +0530

    Time: 0 ms (100%), Space: 41.9 MB (84.54%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
@@ -1,26 +1,20 @@
 class Solution {
-    public static int sol(int n ,int idx,ArrayList<Integer> set){
-        if(idx==n){
-            return 1;
-        }
-        int count=0;
-        for(int i=0;i<=9 ;i++){
-            if(idx==0 && i==0 && n>1){
-                continue;
-            }
-            if(!set.contains(i)){
-                set.add(i);
-                count += sol(n,idx+1,set);
-                set.remove(set.size()-1);    
-            }
-        }
-        return count+1;
-    }
     public int countNumbersWithUniqueDigits(int n) {
-        ArrayList<Integer> set = new ArrayList<>();
-        if(n==1){
-            return sol(n,0,set)-1;
+        
+        if(n==0) return 1;
+
+        int unique=9;
+        int fact=9;
+        int ans=10;
+
+
+        for(int i=2;i<=n && fact>0;i++){
+           unique*=fact;
+           ans+=unique;
+           fact--;
+
         }
-        return sol(n,0,set);
+
+        return ans;
     }
 }
