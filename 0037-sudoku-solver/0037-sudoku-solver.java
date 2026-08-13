69ef9645ab64a8df53a521b8c96ad124fa45da87
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-08-13 08:17:15 +0530

    Time: 167 ms (82.95%), Space: 43 MB (20.6%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,36 +1,48 @@
 class Solution {
-
-    public boolean ispossible(char[][] grid, int row, int col, char val){
+    public boolean isPossible(int row,int col,char board[][],char ch){
         for(int i=0;i<9;i++){
-            if(grid[i][col] == val) return false;
-            if(grid[row][i] == val) return false;
+            if(board[row][i] == ch ){
+                return false;
+            }
+            if(board[i][col] == ch ){
+                return false;
+            }
         }
-        int sr = row - row % 3;
-        int sc = col - col % 3;
+        int sr = row - (row % 3);
+        int sc = col - (col % 3);
         for(int i=sr;i<sr+3;i++){
             for(int j=sc;j<sc+3;j++){
-                if(grid[i][j] == val) return false;
+                if(board[i][j] == ch){
+                    return false;
+                }
             }
         }
         return true;
     }
-
-    public boolean helper(char[][] grid, int row, int col){
-        if(row == 9) return true;
-        if(col == 9) return helper(grid, row+1, 0);
-        if(grid[row][col] != '.') return helper(grid, row, col+1);
-
-        for(char i='1'; i<='9'; i++){
-            if(ispossible(grid, row, col, i)){
-                grid[row][col] = i;
-                if(helper(grid, row, col+1)) return true;
-                grid[row][col] = '.';
+    public boolean solve(int row,int col,char board[][]){
+        if(row >= 9){
+            return true;
+        }
+        if(col>=9){
+            return solve(row+1,0,board);
+        }
+        if(board[row][col] !=  '.'){
+            return solve(row,col+1,board);
+        }
+        for(char ch = '1' ; ch <= '9' ; ch++){
+            if(isPossible(row,col,board,ch)){
+                board[row][col] = ch;
+                if(solve(row,col,board)){
+                    return true;
+                }
+                board[row][col] = '.';
             }
         }
         return false;
     }
-
-    public void solveSudoku(char[][] grid) {
-        helper(grid, 0, 0);
+    public void solveSudoku(char[][] board) {
+        int n = board.length;
+        int m = board[0].length;
+        solve(0,0,board);
     }
 }
