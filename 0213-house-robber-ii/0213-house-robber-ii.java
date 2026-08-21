e3e0fa10320d30933a05b552cdeed1ea959bc00c
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-08-21 11:56:14 +0530

    Time: 0 ms (100%), Space: 42.4 MB (95.31%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,26 +1,29 @@
 class Solution {
-    public int robber(int arr[],int idx,int n,int dp[]){
-        if(idx>=n){
+    public int robbery(int idx,int e,int nums[],int dp[]){
+        if(idx >= e){
             return 0;
         }
         if(dp[idx] != -1){
             return dp[idx];
         }
-        int pick = arr[idx] + robber(arr,idx+2,n,dp);
-        int notPick = robber(arr,idx+1,n,dp);
-        return dp[idx]  = Math.max(pick,notPick);
+        int pick = nums[idx]+robbery(idx+2,e,nums,dp);
+        int notPick = robbery(idx+1,e,nums,dp);
+        return dp[idx] =  Math.max(pick,notPick);
     }
     public int rob(int[] nums) {
         int n = nums.length;
         if(n==1){
             return nums[0];
         }
+        if(n==2){
+            return Math.max(nums[0],nums[1]);
+        }
         int dp1[] = new int[n];
         Arrays.fill(dp1,-1);
-        int case1 = robber(nums,0,n-1,dp1);
+        int case1 = robbery(0,n-1,nums,dp1);
         int dp2[] = new int[n];
         Arrays.fill(dp2,-1);
-        int case2 = robber(nums,1,n,dp2);
+        int case2 = robbery(1,n,nums,dp2);
         return Math.max(case1,case2);
     }
 }
