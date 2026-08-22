df0bc4ecd2cad7d78079fcaad0dc15ff0d7b7492
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-08-22 13:48:09 +0530

    Time: 35 ms (7.21%), Space: 57.2 MB (22.8%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,26 +1,30 @@
 class Solution {
+    public int ways(int idx,int coins[],int amount,int dp[][]){
+        if(amount<0){
+            return 0;
+        }
+        if(amount == 0){
+            return 1;
+        }
+        if(idx >= coins.length){
+            return 0;
+        }
+        if(dp[idx][amount] != -1){
+            return dp[idx][amount];
+        }
+        int count = 0;
+        if(amount-coins[idx] >= 0){
+            count += ways(idx,coins,amount-coins[idx],dp);
+        }
+        count += ways(idx+1,coins,amount,dp);
+        return dp[idx][amount] = count;
+    }
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int dp[][] = new int[n][amount+1];
-        for(int i=0;i<n;i++){
-            dp[i][0] =1;
+        int dp[][] =new int[n][amount+1];
+        for(int row[] : dp){
+            Arrays.fill(row,-1);
         }
-        for(int j=1;j<=amount ;j++){
-            if(j-coins[0] >=0){
-                dp[0][j] = dp[0][j-coins[0]];
-            }
-        }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<=amount ;j++){
-                int pick = j-coins[i];
-                if(pick>=0){
-                    dp[i][j] = dp[i][pick]+dp[i-1][j];
-                }
-                else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-        return dp[n-1][amount];
+        return ways(0,coins,amount,dp);
     }
 }
