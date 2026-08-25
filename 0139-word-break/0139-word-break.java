99bcd7928f5f7cb6081d6649b9be3a19316b2e12
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-08-25 19:37:48 +0530

    Time: 8 ms (49.2%), Space: 46.3 MB (34.83%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,31 +1,17 @@
 class Solution {
-    public boolean isContain(int idx,String s,String str,HashSet<String> set,Boolean dp[]){
-        if(idx == s.length() && str.equals("")){
-            return true;
-        }
-        if(idx >= s.length()){
-            return false;
-        }
-        if(dp[idx] != null){
-            return dp[idx];
-        }
-        for(int i=idx;i<s.length();i++){
-            str += s.charAt(i);
-            if(set.contains(str)){
-                if(isContain(i+1,s,"",set,dp) || isContain(i+1,s,str,set,dp)){
-                    return dp[idx] = true;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        HashSet<String> set = new HashSet<>(wordDict);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
                 }
             }
         }
-        return dp[idx] = false;
-    }
-    public boolean wordBreak(String s, List<String> wordDict) {
-        HashSet<String> set = new HashSet<>();
-        for(String str : wordDict){
-            set.add(str);
-        }
-        int n = s.length();
-        Boolean dp[] = new Boolean[n+1];
-        return isContain(0,s,"",set,dp);
+        return dp[n];
     }
 }
