1f5ca55454dacf80e4aa4e38f629c4d97c7bb123
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-08-31 15:48:40 +0530

    Time: 12 ms (13.99%), Space: 71.1 MB (26.96%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,44 +1,34 @@
 class Solution {
-    public static int helper(int row,int col,char mat[][],int n,int m,int dp[][]){
-        if(row>=n || col>=m){
+    public int helper(int row,int col,char matrix[][],int dp[][]){
+        if(row >= matrix.length || col >= matrix[0].length){
             return 0;
         }
-        if(dp[row][col]!=-1){
+        if(dp[row][col] != -1){
             return dp[row][col];
         }
-        if(mat[row][col] == '1'){
-            int down = helper(row+1,col,mat,n,m,dp);
-            int right = helper(row,col+1,mat,n,m,dp);
-            int dig = helper(row+1,col+1,mat,n,m,dp);
-            return dp[row][col] = 1 + Math.min(down, Math.min(right, dig));
+        if(matrix[row][col] == '1'){
+            int right = helper(row,col+1,matrix,dp); 
+            int down = helper(row+1,col,matrix,dp);
+            int dig = helper(row+1,col+1,matrix,dp);
+            return dp[row][col] = 1+Math.min(right,Math.min(down,dig));
         }
-        else{
-            return dp[row][col] = 0;
-        }
+        return dp[row][col] = 0;
     }
     public int maximalSquare(char[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
         int dp[][] = new int[n][m];
-        for(int row[]:dp){
+        for(int row[] :dp){
             Arrays.fill(row,-1);
         }
+        int max = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j] =='0'){
-                    dp[i][j] = 0;
-                }
-                else{
-                    helper(i,j,matrix,n,m,dp);
+                if(matrix[i][j] == '1'){
+                    max = Math.max(max,helper(i,j,matrix,dp));
                 }
             }
         }
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                ans = Math.max(ans,dp[i][j]); 
-            }
-        }
-        return ans*ans;
+        return max*max;
     }
 }
