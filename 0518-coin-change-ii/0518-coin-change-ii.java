38310ab3acc8e5e97981cc90274e5fcf206990c6
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-09-18 11:48:02 +0530

    Time: 32 ms (11.01%), Space: 56.7 MB (47.12%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,30 +1,30 @@
 class Solution {
-    public int ways(int idx,int coins[],int amount,int dp[][]){
-        if(amount<0){
-            return 0;
-        }
+    public int ways(int idx,int length,int amount,int coins[],int dp[][]){
         if(amount == 0){
             return 1;
         }
-        if(idx >= coins.length){
+        if(amount<0){
+            return 0;
+        }
+        if(idx >= length){
             return 0;
         }
         if(dp[idx][amount] != -1){
             return dp[idx][amount];
         }
-        int count = 0;
-        if(amount-coins[idx] >= 0){
-            count += ways(idx,coins,amount-coins[idx],dp);
+        int count  = 0;
+        if(coins[idx] <= amount){
+            count += ways(idx,length,amount-coins[idx],coins,dp);
         }
-        count += ways(idx+1,coins,amount,dp);
+        count += ways(idx+1,length,amount,coins,dp);
         return dp[idx][amount] = count;
     }
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int dp[][] =new int[n][amount+1];
-        for(int row[] : dp){
+        int dp[][] = new int[n+1][amount+1];
+        for(int row[] :dp){
             Arrays.fill(row,-1);
         }
-        return ways(0,coins,amount,dp);
+        return ways(0,n,amount,coins,dp);
     }
 }
