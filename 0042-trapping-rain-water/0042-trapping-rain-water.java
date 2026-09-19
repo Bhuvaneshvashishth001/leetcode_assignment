52ba94e69eb80c561a595226090763afba134f08
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-09-19 15:22:03 +0530

    Time: 1 ms (56.02%), Space: 47.6 MB (65.95%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,37 +1,31 @@
 class Solution {
     public int trap(int[] arr) {
-        int n = arr.length;
-        int pge[] = new int[n];
-        int nge[] = new int[n];
-        pge[0] = 0;
-        int max = arr[0];
-        for(int i=1;i<n;i++){
-            if(max > arr[i]){
-                pge[i] = max;
+       int  n = arr.length;
+       int l = 0;
+       int r = n-1;
+       int lmax = 0;
+       int rmax = 0;
+       int total = 0;
+       while(l<r){
+            if(arr[l] <= arr[r]){
+                if(lmax >= arr[l]){
+                    total += (lmax-arr[l]);
+                }
+                else{
+                    lmax = arr[l];
+                }
+                l++;
             }
             else{
-                max =arr[i];
-                pge[i] = 0;
+                if(rmax >= arr[r]){
+                    total += (rmax-arr[r]);
+                }
+                else{
+                    rmax = arr[r];
+                }
+                r--;
             }
-        }
-        nge[n-1] = 0;
-        int max1 = arr[n-1];
-        for(int i=n-2;i>=0;i--){
-            if(max1 > arr[i]){
-                nge[i] = max1;
-            }
-            else{
-                max1 = arr[i];
-                nge[i] = 0;
-            }
-        }
-        int total = 0;
-        for(int i =0;i<n;i++){
-            int min = Math.min(pge[i],nge[i]);
-            if(min>arr[i]){
-                total += (min-arr[i]);
-            }
-        }
-        return total;
+       }
+       return total;
     }
 }
