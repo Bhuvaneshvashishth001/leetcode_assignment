63d2095bf8c83a00a2274be6cba51794b2d7bc3c
Bhuvanesh Vashishth <bhuvneshvashishth925@gmail.com>
2026-09-19 23:09:24 +0530

    Time: 3 ms (87.29%), Space: 52.4 MB (19.7%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int candy(int[] arr) {
+        int n  = arr.length;
+        int pre[]  = new int[n];
+        int suff[]  = new int[n];
+        pre[0] = 1;
+        for(int i=1;i<n;i++){
+            if(arr[i] > arr[i-1]){
+                pre[i] = pre[i-1]+1;
+            }
+            else{
+                pre[i] = 1;
+            }
+        }
+        suff[n-1]  = 1;
+        for(int i=n-2 ;i>=0;i--){
+            if(arr[i] > arr[i+1]){
+                suff[i] = suff[i+1]+1;
+            }
+            else{
+                suff[i] = 1;
+            }
+        }
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            ans += Math.max(pre[i],suff[i]);
+        }
+        return ans;
+    }
+}
